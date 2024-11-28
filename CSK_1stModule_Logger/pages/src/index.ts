bf870a96ae1d52f27e0cfcc4b29a9b1a1bb85eea
Roy Gollub <roy.gollub@sick.de>
2024-11-28 08:55:37 +0100

    - Removing navigation bar entries

--- CSK_1stModule_Logger/pages/src/index.ts
+++ CSK_1stModule_Logger/pages/src/index.ts
@@ -3,7 +3,15 @@ const page_1 = document.querySelector('div.sopasjs-ui-navbar-wrapper > div > ul 
 page_1.classList.remove('fa-file');
 page_1.classList.add('fa-comment-o');
 
-setTimeout(() => {
+  const page_FirstLabel = document.querySelector('div.sopasjs-ui-navbar-wrapper > div > ul > li:nth-child(2)');
+  const page_App = document.querySelector('div.sopasjs-ui-navbar-wrapper > div > ul > li:nth-child(4)');
+  const page_Setup = document.querySelector('div.sopasjs-ui-navbar-wrapper > div > ul > li:nth-child(5) > a');
+
+  page_FirstLabel.remove();
+  page_App.remove();
+  page_Setup.remove();
+
+  setTimeout(() => {
     document.title = 'CSK_Module_Logger'
 }, 500);
 })
